75f58104fb6774afaad1abb79f9d4eacfe87144c
aldisypu <aldisypu@gmail.com>
2024-01-01 23:24:43 +0700

    anime app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const animesData = [
     mal_id: 21,
     title: 'One Piece',
     year: 1999,
-    image: 'https://cdn.myanimelist.net/images/anime/6/73245.jpg',
+    image: 'https://cdn.myanimelist.net/images/anime/1244/138851.jpg',
     score: 8.71,
     synopsis:
       'Barely surviving in a barrel after passing through a terrible whirlpool at sea, carefree Monkey D. Luffy ends up aboard a ship under attack by fearsome pirates. Despite being a naive-looking teenager, he is not to be underestimated. Unmatched in battle, Luffy is a pirate himself who resolutely pursues the coveted One Piece treasure and the King of the Pirates title that comes with it.',
@@ -41,11 +41,8 @@ const animesData = [
 ];
 
 export default function App() {
-  const [query, setQuery] = useState('');
   const [animes, setAnimes] = useState(animesData);
   const [selectedAnime, setSelectedAnime] = useState(animes[0]);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
 
   function handleSelectedAnime(id) {
     const newAnime = animes.filter((anime) => anime.mal_id === id);
@@ -54,65 +51,124 @@ export default function App() {
 
   return (
     <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍥</span>
-          <h1>Ihh WeeBoo</h1>
-          <span role="img">👻</span>
-        </div>
-        <div className="search-container">
-          <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
-          <p className="search-results">
-            Found <strong>4</strong> results
+      <NavBar>
+        <Search>
+          <NumResult animes={animes} />
+        </Search>
+      </NavBar>
+      <Main>
+          <Box>
+            <AnimeList animes={animes} onSelectedAnime={handleSelectedAnime} />
+          </Box>
+          <Box>
+            <AnimeDetail selectedAnime={selectedAnime} />
+          </Box>
+      </Main>
+    </>
+  );
+}
+
+function NavBar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍥</span>
+      <h1>Ihh WeeBoo</h1>
+      <span role="img">👻</span>
+    </div>
+  );
+}
+
+function Search({ children }) {
+  const [query, setQuery] = useState('');
+
+  return (
+    <div className="search-container">
+      <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
+      {children}
+    </div>
+  );
+}
+
+function NumResult({ animes }) {
+  return (
+    <p className="search-results">
+      Found <strong>{animes.length}</strong> results
+    </p>
+  );
+}
+
+function Main({ children }) {
+
+  return (
+    <main className="main">
+      { children }
+    </main>
+  );
+}
+
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? '–' : '+'}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+function AnimeList({ animes, onSelectedAnime }) {
+  return (
+    <ul className="list list-anime">
+      {animes?.map((anime) => (
+        <Anime key={anime.mal_id} anime={anime} onSelectedAnime={onSelectedAnime} />
+      ))}
+    </ul>
+  );
+}
+
+function Anime({ anime, onSelectedAnime }) {
+  return (
+    <li  onClick={() => onSelectedAnime(anime.mal_id)}>
+      <img src={anime.image} alt={`${anime.title} cover`} />
+      <h3>{anime.title}</h3>
+      <div>
+        <p>
+          <span>{anime.year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function AnimeDetail({ selectedAnime }) {
+  return (
+    <div className="details">
+      <header>
+        <img src={selectedAnime.image} alt={`${selectedAnime.title} cover`} />
+        <div className="details-overview">
+          <h2>{selectedAnime.title}</h2>
+          <p>
+            {selectedAnime.year} &bull; {selectedAnime.score}
           </p>
         </div>
-      </nav>
-
-      <main className="main">
-        <div className="box">
-          <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
-            {isOpen1 ? '–' : '+'}
-          </button>
-          {isOpen1 && (
-            <ul className="list list-anime">
-              {animes?.map((anime) => (
-                <li key={anime.mal_id} onClick={() => handleSelectedAnime(anime.mal_id)}>
-                  <img src={anime.image} alt={`${anime.title} cover`} />
-                  <h3>{anime.title}</h3>
-                  <div>
-                    <p>
-                      <span>{anime.year}</span>
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className="box">
-          <button className="btn-toggle" onClick={() => setIsOpen2((open) => !open)}>
-            {isOpen2 ? '–' : '+'}
-          </button>
-          {isOpen2 && (
-            <div className="details">
-              <header>
-                <img src={selectedAnime.image} alt={`${selectedAnime.title} cover`} />
-                <div className="details-overview">
-                  <h2>{selectedAnime.title}</h2>
-                  <p>
-                    {selectedAnime.year} &bull; {selectedAnime.score}
-                  </p>
-                </div>
-              </header>
-              <section>
-                <p>
-                  <em>{selectedAnime.synopsis}</em>
-                </p>
-              </section>
-            </div>
-          )}
-        </div>
-      </main>
-    </>
+      </header>
+      <section>
+        <p>
+          <em>{selectedAnime.synopsis}</em>
+        </p>
+      </section>
+    </div>
   );
 }
